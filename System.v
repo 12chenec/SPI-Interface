03039251062370e46d8558d0f95238952bb8b52c
Christal Chen <cc5976@princeton.edu>
2026-07-15 14:16:12 -0500

    Refactor System module with updated parameters

--- System.v
+++ System.v
@@ -1,51 +1,60 @@
-module System(
-    input clk,
-    input rst,
-    input en,
-    input cpol,
-    input cpha,
-   
-    input rw,
-    input [6:0] addr,
-    input [7:0] w_data,
-   
-    // new explicit outputs
-    output [7:0] master_r_data,
-    output [7:0] slave_w_data
+module System (
+    input             hclk,
+    input             rst,
+    input             en,
+    input             rw,
+    input      [6:0]  addr,
+    input      [31:0] w_data,
+    input      [3:0]  transfer_len,
+    input      [2:0]  clk_div_val,
+    input             cpol,
+    input             cpha
     );
- 
+    
     wire sclk;
     wire cs;
     wire mosi;
     wire miso;
-   
-    // Master module
-    Master master(
-        .m_clk(clk),
-        .m_rst(rst),
-        .m_en(en),
-        .m_rw(rw),
-        .m_addr(addr),
-        .m_w_data(w_data),
-        .m_r_data(master_r_data),   // expose Master's received data
-        .m_cpol(cpol),
-        .m_cpha(cpha),
-        .m_miso(miso),
-        .m_sclk(sclk),
-        .m_cs(cs),
-        .m_mosi(mosi)
+    
+    Master master (
+        .m_hclk         (hclk),
+        .m_rst          (rst),
+        .m_en           (en),
+        
+        // testbench to rd_wr_shift
+        .m_rw           (rw),
+        .m_addr         (addr),
+        .m_w_data       (w_data),
+        
+        // testbench to data_rem
+        .m_transfer_len (transfer_len),
+        
+        // tesetbench to sclk_gen
+        .m_clk_div_val  (clk_div_val),
+        
+        // SPI modes
+        .m_cpol         (cpol),
+        .m_cpha         (cpha),
+        
+        // SPI interface
+        .m_miso         (miso),
+        .m_sclk         (sclk),
+        .m_cs           (cs),
+        .m_mosi         (mosi)
     );
-   
-    // Slave module
-    Slave slave(
-        .s_rst(rst),
+    
+    Slave slave (
+        .s_rst (rst),
+        
+        // spi mode parameters
         .s_cpol(cpol),
         .s_cpha(cpha),
+        
+        // spi interface
         .s_sclk(sclk),
-        .s_cs(cs),
+        .s_cs  (cs),
         .s_mosi(mosi),
-        .s_miso(miso),
-        .s_w_data(slave_w_data)       // expose Slave's written data
+        .s_miso(miso)
     );
- 
-Endmodule
+    
+endmodule
