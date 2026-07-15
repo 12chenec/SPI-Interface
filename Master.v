187827c3bb45217683f9ca541a2d47d0db558f78
Christal Chen <cc5976@princeton.edu>
2026-07-15 14:14:34 -0500

    Refactor Master module for SPI interface

--- Master.v
+++ Master.v
@@ -1,142 +1,292 @@
-module Master(
-    input m_clk,
-    input m_rst,
-   
-    // testbench
-    input m_en,
-    input m_rw,
-    input [6:0] m_addr,
-    input [7:0] m_w_data,
-    output reg [7:0] m_r_data,
-   
-    // spi mode parameters
-    input m_cpol,
-    input m_cpha,
-   
-    // spi interface
-    input m_miso,
-    output reg m_sclk,
-    output reg m_cs,
-    output reg m_mosi
+module Master (
+    input               m_hclk,
+    input               m_rst,
+    input               m_en,
+    
+    // testbench to rd_wr_shift
+    input               m_rw,
+    input       [6:0]   m_addr,
+    input       [31:0]  m_w_data,
+    // testbench to data_rem
+    input       [3:0]   m_transfer_len,
+    // tesetbench to sclk_gen
+    input       [2:0]   m_clk_div_val,
+    
+    // SPI modes
+    input               m_cpol,
+    input               m_cpha,
+    
+    // SPI interface
+    input               m_miso,
+    output reg          m_sclk,
+    output reg          m_cs,
+    output reg          m_mosi
     );
-   
+    
     // states
     reg [2:0] m_state, m_next_state;
-    localparam IDLE     = 3'd0;
-    localparam ADDR     = 3'd1;
-    localparam READ     = 3'd2;
-    localparam WRITE    = 3'd3;
-   
-    // internal registers
-    reg [15:0] m_sr;
-    reg [3:0] m_bitcount;
- 
-   
-    // output logic
-    always @(*) begin
-        // SCLK timing based on mode
-        //  m_sclk = (m_cpol) ? ~m_clk : m_clk;
-        m_sclk = m_clk;
-       
-        case (m_state)
-            ADDR, READ, WRITE: begin
-                m_mosi = m_sr[15];
-            end
-            default: begin
-                m_mosi = 0;
-            end
-        endcase
+    localparam IDLE =   3'd0;
+    localparam ADDR =   3'd1;
+    localparam READ =   3'd2;
+    localparam WRITE =  3'd3;
+    
+    // signals from datapath
+    // bit_cnt
+    reg m_byte_done;
+    // data_rem
+    reg m_transfer_done;
+    
+    // signals from controller
+    wire m_drive_shift_load;
+    
+    // registers in datapath
+    // rd_wr_shift
+    reg [7:0] m_sample_shift;
+    reg [7:0] m_drive_shift;
+    reg [31:0] m_r_data;
+    reg [31:0] m_w_data_rem;
+    // bit_cnt
+    reg [3:0] m_bit_cnt;
+    // data_rem
+    reg [3:0] m_data_rem;
+    // sclk_div
+    reg [2:0] m_clk_cnt;
+    reg       m_sclk_in;
+    reg       m_sclk_prev;
+    
+    // ===== DATAPATH =====
+    
+    // === rd_wr_shift ===
+    
+    // sample_shift
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst) begin
+            m_sample_shift <= 8'd0;
+        end
+        else if (m_latch_en) begin
+            m_sample_shift <= {m_sample_shift[6:0], m_miso};
+        end
     end
-   
+    // r_data
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst || m_en) begin
+            m_r_data <= 32'd0;
+        end
+        else if (m_state == READ && m_byte_done && m_cnt_en) begin
+            m_r_data <= {m_r_data[23:0], m_sample_shift};
+        end
+    end
+    // drive_shift
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst || m_cs) begin
+            m_drive_shift <= 8'd0;
+        end
+        else if (m_drive_shift_addr) begin
+            m_drive_shift <= {m_rw, m_addr};
+        end
+        else if (m_drive_shift_write) begin
+                m_drive_shift <= m_w_data_rem[7:0];
+        end
+        else begin
+            if (m_cnt_en) begin
+                m_drive_shift <= {m_drive_shift[6:0], 1'b0};
+            end
+        end
+    end
+    // w_data_rem
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst) begin
+            m_w_data_rem <= 32'd0;
+        end
+        else if (m_en && !m_rw) begin
+            m_w_data_rem <= m_w_data;
+        end
+        else if (m_drive_shift_load) begin
+            m_w_data_rem <= {8'd0, m_w_data_rem[31:8]};
+        end
+    end
+    // mosi
+    always @(*) begin
+        if (m_rst) begin
+            m_mosi = 0;
+        end
+        else if (m_state == ADDR || m_state == WRITE) begin
+            m_mosi = m_drive_shift[7];
+        end
+        else begin
+            m_mosi = 0;
+        end
+    end
+    
+    // === bit_cnt ===
+    // bit_cnt
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst || m_next_state == IDLE || m_state == IDLE) begin
+            m_bit_cnt <= {3'd0, ~m_cpha};
+        end
+        else if (m_byte_done && m_cnt_en) begin
+            m_bit_cnt <= 4'd1;
+        end
+        else if (m_cnt_en) begin
+            m_bit_cnt <= m_bit_cnt + 4'd1;
+        end
+    end
+    // byte_done
+    always @(*) begin
+        if (m_rst) begin
+            m_byte_done = 0;
+        end
+        else if (m_bit_cnt == 4'd8) begin
+            m_byte_done = 1;
+        end
+        else begin
+            m_byte_done = 0;
+        end
+    end
+    
+    // === data_rem ===
+    // data_rem
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst) begin
+            m_data_rem <= 4'd0;
+        end
+        else if (m_state == ADDR && m_next_state != ADDR) begin
+            m_data_rem <= m_transfer_len;
+        end
+        else if (m_byte_done && m_cnt_en) begin
+            m_data_rem <= m_data_rem - 4'd1;
+        end
+    end
+    // transfer_done
+    always @(*) begin
+        if (m_rst) begin
+            m_transfer_done = 0;
+        end
+        else if ((m_state == READ || m_state == WRITE) && m_data_rem == 4'd0) begin
+            m_transfer_done = 1;
+        end
+        else begin
+            m_transfer_done = 0;
+        end
+    end
+    
+    // === sclk_gen ===
+    // clk_cnt
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst || m_state == IDLE || m_clk_cnt == m_clk_div_val) begin
+            m_clk_cnt <= 3'd1;
+        end
+        else begin
+            m_clk_cnt <= m_clk_cnt + 3'd1;
+        end
+    end
+    
+    // sclk_in
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst || m_cs) begin
+            m_sclk_in <= 1'b0;
+        end
+        else if (m_clk_cnt == m_clk_div_val) begin
+            m_sclk_in <= ~m_sclk_in;
+        end
+    end
+    
+    // sclk
+    always @(*) begin
+        if (m_rst || m_cs) begin
+            m_sclk = m_cpol;
+        end
+        else begin
+            m_sclk = m_sclk_in ^ m_cpol;
+        end
+    end
+    
+    // sclk_prev
+    always @(posedge m_hclk or m_rst) begin
+        if (m_rst) begin
+            m_sclk_prev <= 1'b0;
+        end
+        else begin
+            m_sclk_prev <= m_sclk_in ^ m_cpol;
+        end
+    end
+    
+    assign m_falling_edge = ~(m_sclk_in ^ m_cpol) & m_sclk_prev;
+    assign m_rising_edge = (m_sclk_in ^ m_cpol) & ~m_sclk_prev;
+    
+    assign m_latch_en = (m_cpol ^ m_cpha) ? m_falling_edge : m_rising_edge;
+    assign m_cnt_en = (m_cpol ^ m_cpha) ? m_rising_edge : m_falling_edge;
+    
+    // ===== CONTROLLER =====
+    
+    // drive_shift_init
+    assign m_drive_shift_addr = !m_cs && !m_cnt_en && m_bit_cnt == 3'd1 && m_state == ADDR;
+    // drive_shift_load
+    assign m_drive_shift_write = ((m_state == ADDR && !m_rw) || m_state == WRITE) && m_byte_done && m_cnt_en;
+    
+    // cs
+    always @(*) begin
+        if (m_rst || m_state == IDLE) begin
+            m_cs = 1;
+        end
+        else begin
+            m_cs = 0;
+        end
+    end
+    
+    // current state logic
+    always @(posedge m_hclk or posedge m_rst) begin
+        if (m_rst) begin
+            m_state <= IDLE;
+        end
+        else begin
+            m_state <= m_next_state;
+        end
+    end
     
     // next state logic
     always @(*) begin
-        case (m_state)
-            IDLE: begin
-                m_next_state = m_en ? ADDR : IDLE;
-            end
-            ADDR: begin
-                m_next_state = (m_bitcount == 7) ? (m_rw ? READ : WRITE) : ADDR;
-            end
-            READ: begin
-                m_next_state = (m_bitcount == 15) ? IDLE : READ;
-            end
-            WRITE: begin
-                m_next_state = (m_bitcount == 15) ? IDLE : WRITE;
-            end
-            default: begin
-                m_next_state = IDLE;
-            end
-        endcase
-    end
-   
-    // sequential logic
-   
-    // internal clock for sampling/shifting
-    wire m_in_clk;
-    assign m_in_clk = (m_cpol ^ m_cpha) ? ~m_clk : m_clk;
-   
-    // negedge state update
-    always @(negedge m_clk or posedge m_rst) begin
         if (m_rst) begin
-            m_bitcount <= 0;
-            m_state <= IDLE;
-            m_sr <= 0;
-        end else begin
-           
-            m_state <= m_next_state;
-       
+            m_next_state = IDLE;
+        end
+        else begin
             case (m_state)
                 IDLE: begin
-                    m_bitcount <= 0;
-                    m_sr <= {m_rw, m_addr, (m_rw ? 8'h00 : m_w_data)};
+                    if (m_en) begin
+                        m_next_state = ADDR;
+                    end
+                    else begin
+                        m_next_state = IDLE;
+                    end
                 end
                 ADDR: begin
-                    m_bitcount <= m_bitcount + 1;
-                    m_sr <= {m_sr[14:0], 1'b0};
+                    if (m_rw && m_cnt_en && m_byte_done) begin
+                        m_next_state = READ;
+                    end
+                    else if (!m_rw && m_cnt_en && m_byte_done) begin
+                        m_next_state = WRITE;
+                    end
+                    else begin
+                        m_next_state = ADDR;
+                    end
                 end
                 READ: begin
-                    m_bitcount <= m_bitcount + 1;
-                    m_sr <= {m_sr[14:0], 1'b0};
+                    if (m_transfer_done && m_cnt_en) begin
+                        m_next_state = IDLE;
+                    end
+                    else begin
+                        m_next_state = READ;
+                    end
                 end
                 WRITE: begin
-                    m_bitcount <= m_bitcount + 1;
-                    m_sr <= {m_sr[14:0], 1'b0};
-                end
-                default: begin
-                    m_bitcount <= 0;
+                    if (m_transfer_done && m_cnt_en) begin
+                        m_next_state = IDLE;
+                    end
+                    else begin
+                        m_next_state = WRITE;
+                    end
                 end
             endcase
         end
     end
- 
-    // sequential logic
-    always @(posedge m_in_clk or posedge m_rst) begin
-        if (m_rst) begin
-            m_cs <= 1;
-            m_r_data <= 0;
-           
-        end else begin
-           
-            case (m_state)
-                IDLE: begin
-                    m_cs <= 1;
-                end
-                ADDR: begin
-                    m_cs <= 0;
-                end
-                READ: begin
-                    m_r_data <= {m_r_data[6:0], m_miso};
-                    m_cs <= 0;
-                end
-                WRITE: begin
-                    m_cs <= 0;
-                end
-                default: begin
-                    m_cs <= 1;
-                end
-            endcase
-        end
-    end
-   
 endmodule
